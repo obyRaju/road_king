5477cb4128109351139edbe8df299fabf3f64d82
obyRaju <obyraju@gmail.com>
2023-11-16 15:45:43 +0530

    bug fix

--- Booking.php
+++ Booking.php
@@ -13,31 +13,52 @@ $mail = new PHPMailer;
 
 $name = !empty($_POST['name'])?$_POST['name']:'';
 $phone = !empty($_POST['phone'])?$_POST['phone']:'';
-$phone = !empty($_POST['message'])?$_POST['message']:'';
+$msg = !empty($_POST['message'])?$_POST['message']:'';
+$location = !empty($_POST['location'])?$_POST['location']:'';
 
-$emailBody = "";
+$emailBody = "<!DOCTYPE html>
+<html>
+<body>
+    <div>
+        <section>
+            <p>Dear sir/madam,</p>
+            <div>
+            $msg
+                <br><br>
+                <div>
+                <p>Best regards, </p>
+                </div>
+                <p>$name</p>
+                <p>$phone</p>
+                <p>$location</p>
+            </div>
+        </section>
+    </div>
+</body>
+</html>";
+
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'in-v3.mailjet.com';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'b925f3523b855aa9dd55bd40aacf9343';                 // SMTP username
-$mail->Password = '47bb7cf81b7aa9e99d46361e60993db7';                           // SMTP password
-$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587;                                    // TCP port to connect to
+ $mail->isSMTP();                                      // Set mailer to use SMTP
+ $mail->Host = 'smtp-relay.sendinblue.com';  // Specify main and backup SMTP servers
+ $mail->SMTPAuth = true;                               // Enable SMTP authentication
+ $mail->Username = 'support@safelimogcc.com';                 // SMTP username
+ $mail->Password = 'OWBgJxGjynaV2UTf';                           // SMTP password
+ $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+ $mail->Port = 587;                                    // TCP port to connect to
 
-$mail->From = 'oby@alignminds.com';
-$mail->FromName = 'Oby';
-$mail->addAddress('obyraju@gmail.com', 'oby');     // Add a recipient
-$mail->addReplyTo('oby@alignminds.com', 'Information');
-$mail->addCC('oby@alignminds.com');
-$mail->addBCC('oby@alignminds.com');
+ $mail->From = 'support@safelimogcc.com';
+ $mail->FromName = 'Safe limo';
+ $mail->addAddress('obyraju@gmail.com', 'oby');     // Add a recipient
+ //$mail->addReplyTo($email, $name);
+ $mail->addCC('support@safelimogcc.com');
+ $mail->addBCC('support@safelimogcc.com');
 
-$mail->WordWrap = 50;                                 // Set word wrap to 50 characters
-$mail->isHTML(true);                                  // Set email format to HTML
+ $mail->WordWrap = 50;                                 // Set word wrap to 50 characters
+ $mail->isHTML(true);                                  // Set email format to HTML
 
-$mail->Subject = 'Booking';
-$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+ $mail->Subject = 'Booking - Safe Limo';
+ $mail->Body    =  $emailBody;
 //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
 if(!$mail->send()) {
--- contact.php
+++ contact.php
@@ -13,32 +13,51 @@ $mail = new PHPMailer;
 
 $name = !empty($_POST['name'])?$_POST['name']:'';
 $phone = !empty($_POST['Phone_number'])?$_POST['Phone_number']:'';
-$phone = !empty($_POST['Msg'])?$_POST['Msg']:'';
+$message = !empty($_POST['Msg'])?$_POST['Msg']:'';
 $email = !empty($_POST['email'])?$_POST['email']:'';
-$emailBody = "";
+$emailBody = "<!DOCTYPE html>
+<html>
+<body>
+    <div>
+        <section>
+            <p>Dear sir/madam,</p>
+            <div>
+            $message
+                <br><br>
+                <div>
+                <p>Best regards, </p>
+                </div>
+                <p>$name</p>
+                <p>$phone</p>
+                <p></p>
+            </div>
+        </section>
+    </div>
+</body>
+</html>";
 
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
  $mail->isSMTP();                                      // Set mailer to use SMTP
- $mail->Host = 'in-v3.mailjet.com';  // Specify main and backup SMTP servers
+ $mail->Host = 'smtp-relay.sendinblue.com';  // Specify main and backup SMTP servers
  $mail->SMTPAuth = true;                               // Enable SMTP authentication
- $mail->Username = '2862fcc22d1dbfff2d2543bafad0aa28';                 // SMTP username
- $mail->Password = 'b6aa2b7dc25a3923af6d5a67f98d9692';                           // SMTP password
+ $mail->Username = 'support@safelimogcc.com';                 // SMTP username
+ $mail->Password = 'OWBgJxGjynaV2UTf';                           // SMTP password
  $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
  $mail->Port = 587;                                    // TCP port to connect to
 
- $mail->From = 'oby@alignminds.com';
- $mail->FromName = 'Oby';
+ $mail->From = $email;
+ $mail->FromName = $name;
  $mail->addAddress('obyraju@gmail.com', 'oby');     // Add a recipient
- $mail->addReplyTo('support@safelimogcc.com', 'Safe limo');
+ $mail->addReplyTo($email, $name);
  $mail->addCC('support@safelimogcc.com');
  $mail->addBCC('support@safelimogcc.com');
 
  $mail->WordWrap = 50;                                 // Set word wrap to 50 characters
  $mail->isHTML(true);                                  // Set email format to HTML
 
- $mail->Subject = 'Booking';
- $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+ $mail->Subject = 'Inquiry/Feedback/Support - Safe Limo';
+ $mail->Body    =  $emailBody;
  //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
  if(!$mail->send()) {
